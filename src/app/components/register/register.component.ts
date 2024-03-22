c9dcbb5826bfacac6561d9b63e32391796c53e35
José Daniel Lara <larajosed@gmail.com>
2024-03-22 12:12:35 +0100

    new version of the form and creation of the formGroup

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,26 +1,29 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { UserService } from '../../service/user.service';
-import { Router } from 'express';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { SubTitleComponent } from '../sub-title/sub-title.component';
+import { MasterTableService } from '../../service/masterTableService';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
+  imports: [SubTitleComponent, ReactiveFormsModule],
 })
 export class RegisterComponent {
-  name = new FormControl('');
-  lastName = new FormControl('');
-  email = new FormControl('');
-  password = new FormControl('');
-  gender = new FormControl();
-  address =  new FormControl();
-  role = new FormControl();
-  date = new FormData();
+  form: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    lastName: new FormControl(''),
+    gender: new FormControl(''),
+    birthdate: new FormControl(),
+    address: new FormControl(''),
+    postalCode: new FormControl(''),
+    location: new FormControl(''),
+    province: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    role: new FormControl(''),
+  });
 
-constructor(){
-
-}
+  constructor(private masterTableService: MasterTableService) {}
 }
