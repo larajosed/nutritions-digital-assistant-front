e3f4cfa88377fed086d1d07475640d5c5673b034
José Daniel Lara <larajosed@gmail.com>
2024-03-26 10:13:39 +0100

    creation of validators for the registry and the method to save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
 })
 export class AppComponent {
   title = 'nutritions-digital-assistant';
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,10 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private url = 'http://localhost:8080';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  saveUser(user: any) {
+    return this.http.post(`${this.url}/save/user`, user);
+  }
 }
